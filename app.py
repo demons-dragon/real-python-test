250e62c67a287ab43c405cf905d5895ebdf1529a
Demons Dragon <hung.pham@myy.haaga-helia.fi>
2015-07-29 12:48:37 +0300

    flask-hello-world2

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
 from flask import Flask
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
 @app.route("/")
 @app.route("/hello")
 def hello_world():
 	return "Hello, World!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+#integer path
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value+1
+	return "correct"
+
+#float path
+@app.route("/float/<float:value>")
+def int_float(value):
+	print value+1
+	return "correct"
+
+#string route
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "demons-dragon":
+		return "Hello Mr. {}".format(name), 200
+	else:
+		return "Not found ", 404
 
 if __name__ == "__main__":
 	app.run()
